b8ba27eb194ca01ad2e7fcf6e0328e0a95de4651
Ryan Kadel <ryan.kadel88@gmail.com>
2021-02-25 20:53:01 -0500

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7bf321226d7642142cc1eabc3eb03bfd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a8386790-bbc5-4f91-89e7-281dbe95f014'
+};
